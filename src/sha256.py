80635cf02d48cea305e4834cd25432a91dfab42c
Nilan Ziegler <nilan.zieglerlaurent@gmail.com>
2026-05-26 22:06:53 +0200

    Add assertion in S0 method to validate input type and length

--- src/sha256.py
+++ src/sha256.py
@@ -165,6 +165,8 @@ class SHA256:
         Returns:
             str: The resulting 32-bit binary string after applying the S0 function to the input w.
         """
+        assert isinstance(w, str) and len(w) == 32
+        
         return self.xor(self.xor(self.rotate_right(w, 2), self.rotate_right(w, 13)), self.rotate_right(w, 22))
 
     def S1(self, w):
